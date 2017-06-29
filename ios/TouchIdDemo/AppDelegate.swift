ae24a53a7d5bb9692f98c66fe76bff0cc34a534b
Stephen Wingchi Wong <stephwwong@gmail.com>
2017-06-29 12:03:06 -0400

    Converts project to Swift (#2)

---
+++ ios/TouchIdDemo/AppDelegate.swift
@@ -0,0 +1,37 @@
+//
+//  AppDelegate.swift
+//  TouchIdDemo
+//
+//  Created by Stephen Wong on 6/28/17.
+//  Copyright © 2017 Facebook. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+  struct Constants {
+    static let jsBundleRoot = "index.ios"
+    static let moduleName = "TouchIdDemo"
+  }
+  
+  var window: UIWindow?
+  var bridge: RCTBridge!
+  
+  func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey : Any]? = nil) -> Bool {
+    let jsCodeLocation = RCTBundleURLProvider.sharedSettings().jsBundleURL(forBundleRoot: Constants.jsBundleRoot, fallbackResource: nil)
+    var rootView = RCTRootView(bundleURL: jsCodeLocation, moduleName: Constants.moduleName, initialProperties: nil, launchOptions: launchOptions)
+    rootView?.backgroundColor = .white
+    bridge = rootView?.bridge
+    
+    window = UIWindow(frame: UIScreen.main.bounds)
+    
+    let rootViewController = UIViewController()
+    rootViewController.view = rootView
+    
+    window?.rootViewController = rootViewController
+    window?.makeKeyAndVisible()
+    
+    return true
+  }
+}
